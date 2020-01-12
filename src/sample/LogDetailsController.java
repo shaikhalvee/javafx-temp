04d752f40d2221265cf5bf0750513051f72e05d4
Shaikh Islam <shaikhalvee@gmail.com>
2020-01-12 21:12:35 +0600

    file added

--- src/sample/LogDetailsController.java
+++ src/sample/LogDetailsController.java
@@ -1,4 +1,46 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Window;
+
 public class LogDetailsController {
+
+	@FXML
+	public TextField systemReferenceId;
+
+	@FXML
+	public Button searchButton;
+
+	@FXML
+	public TextField createdDate;
+
+	@FXML
+	public TextField sourceImgUri;
+
+	@FXML
+	public TextField targetImgUri;
+
+	@FXML
+	public TextField matchPercentage;
+
+	@FXML
+	public TextArea userReqData;
+
+	@FXML
+	public TextArea rawResponse;
+
+	protected void handleSubmitButtonAction(ActionEvent actionEvent) {
+		Window owner = searchButton.getScene().getWindow();
+		if (systemReferenceId.getText().isEmpty()) {
+			AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Error!",
+					"Please enter System Reference Id.");
+			return;
+		}
+
+	}
 }
